f020e89bf5ad042d6f66ce464c40628522eccefc
Laysinity <160321202+Laysinity@users.noreply.github.com>
2024-06-02 22:03:12 +0500

    update

--- main.py
+++ main.py
@@ -3,24 +3,31 @@ from utils.telegram import Accounts
 from utils.blum import Blum
 from data.config import hello,USE_PROXY
 import asyncio
-
+import os
 
 async def main():
     print(hello)
     action = int(input('Выберите действие:\n1. Начать сбор монет\n2. Создать сессию\n>'))
-
+    
+    if not os.path.exists('sessions'):
+        os.mkdir('sessions')
+    
     if action == 2:
         await create_sessions()
 
     if action == 1:
         accounts = await Accounts().get_accounts()
+        proxy_dict = {}
         with open('proxy.txt','r') as file:
-            proxy = [i.strip() for i in file.readlines()]
+            proxy = [i.strip().split() for i in file.readlines() if len(i.strip().split()) == 2]
+            for prox,name in proxy:
+                proxy_dict[name] = prox
+                
         tasks = []
         if USE_PROXY:
             for thread, account in enumerate(accounts):
-                if len(proxy) > thread:
-                    tasks.append(asyncio.create_task(Blum(account=account, thread=thread, proxy=proxy[thread]).main()))
+                if account in proxy_dict:
+                    tasks.append(asyncio.create_task(Blum(account=account, thread=thread, proxy=proxy_dict[account]).main()))
                 else:
                     tasks.append(asyncio.create_task(Blum(account=account, thread=thread,proxy = None).main()))
         else:
@@ -28,8 +35,5 @@ async def main():
                 tasks.append(asyncio.create_task(Blum(account=account, thread=thread,proxy = None).main()))
         await asyncio.gather(*tasks)
 
-
-
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(main())
-
